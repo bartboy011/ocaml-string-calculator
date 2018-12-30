91d57cf20d73b9261843c554414e63c9dff179c4
Joshua Bartlett <joshua.r.bartlett@gmail.com>
2018-12-30 13:05:48 -0600

    Hey maybe this is done

--- stringcalculator.ml
+++ stringcalculator.ml
@@ -1,4 +1,4 @@
-open Str;;
+let header_regex = Str.regexp "//\\(.*?\\)\n"
 
 let rec sum list =
   match list with
@@ -11,5 +11,13 @@ let sum_str_list str_list =
   | head::[] -> int_of_string head
   | head::list -> sum (List.map (fun x -> int_of_string x) (head :: list))
 
+let has_header str =
+  Str.string_match header_regex str 0
+
+let extract_delimeter str =
+  if has_header str then Str.matched_group 1 str else "[,\n]"
+
 let calc str =
-  sum_str_list (Str.split (regexp ",") str)
+  (* Produces a regex along the lines of /[,\n]/ *)
+  let regex = Str.regexp (Printf.sprintf "%s" (extract_delimeter str)) in
+  sum_str_list (Str.split regex (Str.global_replace header_regex "" str))
--- test/teststringcalculator.ml
+++ test/teststringcalculator.ml
@@ -7,10 +7,22 @@ let one_value_test () =
 let two_value_test () =
   Alcotest.(check int) "It returns 5 when given the string '2,3'" 5 (Stringcalculator.calc("2,3"))
 
+let three_value_test () =
+  Alcotest.(check int) "It returns 15 when given the string '4,5,6'" 15 (Stringcalculator.calc("4,5,6"))
+
+let newline_delimiter_test () =
+  Alcotest.(check int) "It returns 10 when given the string '5\n5'" 10 (Stringcalculator.calc("5\n5"))
+
+let header_delimiter_test () =
+  Alcotest.(check int) "It returns the correct value when a new delimiter is defined in the header" 10 (Stringcalculator.calc("//;\n5;5"))
+
 let main_set = [
   "Empty string test", `Quick, empty_string_test;
   "One value test", `Quick, one_value_test;
   "Two value test", `Quick, two_value_test;
+  "Three value test", `Quick, three_value_test;
+  "Newline delimeter test", `Quick, newline_delimiter_test;
+  "Header delimeter test", `Quick, header_delimiter_test;
 ]
 
 let () =
